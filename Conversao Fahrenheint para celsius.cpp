bb8c00d68f6870ff34fc9aa74ae39d9b3bcf862c
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-26 12:14:57 -0300

    Create Conversao Fahrenheint para celsius.cpp

---
+++ Conversao Fahrenheint para celsius.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace::std;
+int main() {
+cout << "Conversão de fahrenheint em Celsius!\n";
+cout <<"100 gráus em fahrenheint em Celsius equivalem a: "; 
+cout << (100 - 32) / (9) * 5;
+cout << "C*";
+} 
