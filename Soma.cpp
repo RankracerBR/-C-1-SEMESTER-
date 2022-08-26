d6561941f674a913eb2cfa3e09f7976ebbde6c1f
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-26 13:28:28 -0300

    Create Soma.cpp

---
+++ Soma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace::std;
+
+int main() {
+float números1 = 0;
+float números2 = 0;
+
+  cout << "Digite um número inteiro: ";
+cin >> números1;
+
+  cout << "Digite outro número inteiro: ";
+  cin >> números2;
+ 
+  float soma = números1 + números2;
+  
+  cout << "A soma deu: " << soma; 
+}
