685b6338f9b6af1c94d16fcd43ad666741835d1a
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-27 07:58:59 -0300

    Create Romeu e Julieta.cpp

---
+++ Romeu e Julieta.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unistd.h>
+using namespace::std; 
+// 3 romeu
+// 5 julieta
+// 3 e 5 romeu e julieta 
+
+int main() {
+int contador = 0; 
+int maximo = 100;  
+  while(contador < 100){ 
+  contador++;
+  if(contador % 15 == 0){
+    cout << "Romeu e Julieta\n";
+    }else if(contador % 3 == 0){ //modulo(se o numero for multiplo de 3 ele irá ser zero, pois não terá o resto da divisão)
+    cout << "Romeu\n"; 
+    }else if(contador % 5 == 0){
+    cout << "Julieta\n";
+    }else{
+    cout << "Nenhum dos dois" << endl; 
+    }  
+    cout << contador << endl; 
+    sleep(0);
+    }
