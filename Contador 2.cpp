b9e331cd777b377cfb14c5cfb812e43d26ac7d96
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-27 07:50:22 -0300

    Create Contador 2.cpp

---
+++ Contador 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace::std;
+
+int main() {
+  int contador = 100;
+  while(contador >= 0){
+  cout << contador << endl;  
+  contador = contador -2;
+    }
+  cout << "Fora do laço\n";
+  }
+
+
+// :3
