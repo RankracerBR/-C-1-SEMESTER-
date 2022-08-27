53f03d9cea80ae00b2bc9c00e1da921ec1c9d679
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-27 00:49:06 -0300

    Create Compra Produto.cpp

---
+++ Compra Produto.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace::std;
+
+int main() {
+float preco_produto = 100.00;
+float cartao_credito = 150.0;
+float dinheiro = 50.0; 
+int estoque = 0;
+  
+if((cartao_credito > preco_produto or dinheiro > preco_produto) and estoque > 0){ 
+cout << "Você produto comprar o produto! :3" << endl;
+}else{ 
+cout << "Você não pode comprar o produto >:3" << endl; 
+  
+  
+  }
+}
