49ddf1409eed0f5a7fa15a5bdf011ce26c3107da
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-27 08:22:24 -0300

    Create Letras 2.cpp

---
+++ Letras 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace::std;
+
+int main(){
+string palavra = "Araraquara";
+int contador = 0;
+  string upper = "";
+for(int i = 0; i < palavra.size(); i++){ 
+  cout << (char) toupper(palavra[i]) << " ";
+  }
+cout << endl;
+for(int i = palavra.size() - 1; i >= 0; i--){
+  cout << palavra[i] << " ";
+  
+}
+cout << endl;
+for(int i = 0; i < palavra.size(); i++){
+  if(palavra[i] == 'a'){
+    contador++;
+  }
+}
+cout << ""  
+  }
