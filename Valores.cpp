dc69fb892e9e63ebb1378535bc841afd361aae22
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-27 01:15:48 -0300

    Create Valores.cpp

---
+++ Valores.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+/////////////////////// Augusto
+using namespace::std;
+/////////////////////// Pontes
+int main() {
+float valor = 0;
+float A = 0;
+float B = 0; 
+float C = 0;
+float D = 0;
+
+cout << "Digite o valor de A: ";
+cin >> A;
+  
+cout << "Digite o valor de B: ";  
+cin >> B;  
+
+cout << "Digite o valor de C: ";  
+cin >> C;
+
+cout << "Digite o valode de D: ";  
+cin >> D;  
+  
+valor = (A - B) / (C + D); 
+  
+cout << "O valor é: " << valor << endl;   
+  }
