42ef8e946cb5a7d9ea63a8d1cc6f5804d258503e
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-27 00:37:42 -0300

    Create Valor Positivo_ Valor Negativo.cpp

---
+++ Valor Positivo_ Valor Negativo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace::std;
+int main() {
+int valor = 0;
+cout << "Informe um valor: ";   
+cin >> valor; 
+if(valor >= 0){
+cout << "O valor é positivo\n";
+
+}else{
+  cout << "O valor é negativo\n"; 
+   }
+  }
