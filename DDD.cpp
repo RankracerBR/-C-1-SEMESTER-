8e4b4198d9502ae9adfb9e5a8927b116c1c61a51
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-27 01:13:42 -0300

    Create DDD.cpp

---
+++ DDD.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace::std;
+
+int main() {
+int DDD = 0;
+cin >> DDD;  
+if(DDD == 61){
+cout << "Brasilia\n";
+} 
+else if(DDD == 71){ 
+  cout << "Salvador\n";
+}else if(DDD == 11){ 
+cout << "São Paulo";
+}else{   
+  cout << "DDD nao cadastrado\n";  
+  }  
+ }
