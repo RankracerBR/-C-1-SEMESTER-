2b757c912dd7a4d31b2e8b38957528523075e7bc
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-28 13:20:44 -0300

    Create Tabuada.cpp

---
+++ Tabuada.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace::std;
+
+int main() {
+
+int numero;
+
+cout << "Digite um número: ";
+cin >> numero;
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 1: " << numero * 1 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 2: " << numero * 2 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 3: " << numero * 3 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 4: " << numero * 4 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 5: " << numero * 5 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 6: " << numero * 6 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 7: " << numero * 7 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 8: " << numero * 8 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 9: " << numero * 9 << endl;
+ }
+
+for(int i = 0; i <= 0; i++){
+cout << "Tabuada do 10: " << numero * 10 << endl;
+ }
+
+
+
+
+
+  
+}
