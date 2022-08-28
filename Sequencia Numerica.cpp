f8b70e446820a4b56a0056f37082fd2118df2258
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2022-08-28 13:19:12 -0300

    Create Sequencia Numerica.cpp

---
+++ Sequencia Numerica.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace::std;
+
+int main() { 
+  
+int numero;
+
+cout << "Digite um número: ";
+cin >> numero;
+
+for(int i = 1; i <= numero; i++){
+   for(int j = 1; j <= numero; j++){
+     
+     cout << i;
+}
+cout << endl;
+  }
+  
+  
+  }
